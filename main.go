1999f6d89f0e39837c7722189e3d2212cad8ed82
Budi Santoso <budisantoso@Budis-MacBook-Pro.local>
2022-04-28 15:12:56 +0700

    added sort

--- main.go
+++ main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	// "fmt"
 	"fmt"
-	"go_play/database"
+	"sort"
 )
 
-// type Person struct {
-// 	name string
-// }
+type Person struct {
+	name string
+	age  int
+}
 
-// func changeName(p *Person) {
-// 	(*p).name = "santoso"
-// }
+type Persons []Person
+
+func (self Persons) Len() int {
+	return len(self)
+}
+
+func (self Persons) Less(i, j int) bool {
+	return self[i].age < self[j].age
+}
+
+func (self Persons) Swap(i, j int) {
+	self[i], self[j] = self[j], self[i]
+}
 
 func main() {
-	dbName := database.GetDB()
-
-	fmt.Println(dbName)
+	users := Persons{
+		Person{name: "a", age: 5},
+		Person{name: "b", age: 2},
+		Person{name: "d", age: 3},
+	}
+	fmt.Println(users)
+	sort.Sort(Persons(users))
+	fmt.Println(users)
 }
