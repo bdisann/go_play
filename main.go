71a0316398a29a20c9f5265fa975c56d25d1e83e
Budi Santoso <budisantoso@Budis-MacBook-Pro.local>
2022-04-28 16:23:38 +0700

    added time

--- main.go
+++ main.go
@@ -1,37 +1,21 @@
 package main
 
 import (
-	// "fmt"
 	"fmt"
-	"sort"
+	"time"
 )
 
-type Person struct {
-	name string
-	age  int
-}
-
-type Persons []Person
-
-func (self Persons) Len() int {
-	return len(self)
-}
-
-func (self Persons) Less(i, j int) bool {
-	return self[i].age < self[j].age
-}
-
-func (self Persons) Swap(i, j int) {
-	self[i], self[j] = self[j], self[i]
-}
+// "fmt"
 
 func main() {
-	users := Persons{
-		Person{name: "a", age: 5},
-		Person{name: "b", age: 2},
-		Person{name: "d", age: 3},
-	}
-	fmt.Println(users)
-	sort.Sort(Persons(users))
-	fmt.Println(users)
+	now := time.Now()
+
+	fmt.Println(now)
+	fmt.Println(now.Year())
+	fmt.Println(now.Month())
+	fmt.Println(now.Day())
+	fmt.Println(now.Hour())
+	fmt.Println(now.Minute())
+	fmt.Println(now.Second())
+	fmt.Println(now.Nanosecond())
 }
